23e11e421c2495c0db00fc68a43d9bddbdd0857f
Thiago Sousa <thiagosousa81@gmail.com>
2023-10-26 21:37:04 -0300

    §

---
+++ applications/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Aplicações - EBS Seurity Systems</title>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="/images/2º ÌCONE EBS-SYSTEM v2.1 -not shadow.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="/css/styl.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <style>
+      .text-justify {
+        text-align: justify;
+      }
+      
+      .bg-light-gray {
+        background-color: lightgray;
+      }
+      .bg-white {
+        background-color: white;
+        padding-bottom: 5px;
+      }
+    </style>
+  </head>
+  <body>
+    <!--A aplicação será mostrada aqui-->
+    <header id="header"></header>
+    <section id="navbar"></section>
+    
+    <section id="applications" class="jumbotron text-center mycss slide" style="margin-block: 0px; padding-block: 5px">
+        <h3 style="
+                text-align: center;
+                ">Acesso ilimitado às nossas aplicações</h3>
+        <h1 style="background-image: linear-gradient(0deg, black, green,black);"> EBS <img
+                src="https://ebs-systems.epizy.com/images/cropped-imgfinal.png"> WEB</h1>
+        <code style="
+                   -webkit-text-size-adjust: 125%;
+                   -webkit-tap-highlight-color: rgba(0,0,0,0);
+                   line-height: 1.42857143;
+                   text-align: center;
+                   box-sizing: border-box;
+                   font-family: Menlo,Monaco,Consolas,&quot;Courier New&quot;,monospace;
+                   padding: 2px 4px;
+                   font-size: 100%;
+                   color: green;
+                   background-color: #f9f2f4;
+                   border-radius: 4px;
+                   ">Encryption's Builder Studio na web!</code>
+        <div style="
+    margin-top: 5px;
+"><a href="https://api-ebs-web.epizy.com/login"><button type="button"
+                    class="btn btn-secondary"><strong>LOGIN</strong></button></a> <a
+                href="https://api-ebs-web.epizy.com/singup"><button type="button"
+                    class="btn btn-secondary"><strong>CADASTRE-SE</strong></button></a> <a
+                href="https://idc.ebs-systems.epizy.com/"><button type="button"
+                    class="btn btn-secondary"><strong>COMUNIDADE</strong></button></a></div>
+
+    
+    </section>
+    
+    
+    <section id="tercery-message" class="container-fluid" style="background-color: black; padding: 2%">
+        <h1 class="text-center text-light">Como podemos impulsionar o seu negócio?</h1>
+        <div class="container-fluid row">
+            <!-- Bloco 1 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Website Corporativo</h2>
+                <p class="text-justify">
+                    Apresente o seu negócio aos clientes com um website institucional, fornecendo informações de contato e páginas de conversão.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 2 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Agenda Online</h2>
+                <p class="text-justify">
+                    Gerencie os horários de forma eficiente com um sistema de agendamento automatizado, perfeito para barbearias, salões de beleza e lava-jatos.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 3 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Loja Virtual</h2>
+                <p class="text-justify">
+                    Venda seus produtos online com uma loja integrada a cadastros de clientes e produtos flexíveis.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 4 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Sistemas de Gestão</h2>
+                <p class="text-justify">
+                    Tenha um sistema de informação gerencial eficaz para cadastrar clientes, produtos, serviços e compras.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 5 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Integração entre Aplicações</h2>
+                <p class="text-justify">
+                    Acesse seus dados de aplicativos em dispositivos móveis com integração entre todos os nossos sistemas.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 6 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Aplicações de Mídias Sociais</h2>
+                <p class="text-justify">
+                    Crie redes sociais personalizadas, fóruns e comunidades online com recursos de interação social e segurança de assinatura digital.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 7 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Automações com Arduino</h2>
+                <p class="text-justify">
+                    Desenvolvemos produtos integrados a banco de dados online e equipamentos específicos para suas necessidades.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 8 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Automações para Web</h2>
+                <p class="text-justify">
+                    Automatize processos web, como transferência de dados do Excel para sistemas web e muito mais.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 9 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Desenvolvimento e Integração com APIs</h2>
+                <p class="text-justify">
+                    Tenha sua própria API na web e integre nossos sistemas com outras APIs, garantindo a confidencialidade dos dados.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+        </div>
+    </section>
+     
+     <footer id="footer" class="dark-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <h3 class="footer-heading">Encryption's Builder Studio<br>Security Systems</h3>
+            <p class="footer-description">Trabalhando árduamente pela segurança cibernética.</p>
+            <!-- Place this tag where you want the button to render. -->
+<a class="github-button" href="https://github.com/EBS-Security-Systems" data-color-scheme="no-preference: dark_high_contrast; light: dark_high_contrast; dark: dark_high_contrast;" data-size="large" aria-label="Follow @EBS-Security-Systems on GitHub">Follow @EBS-Security-Systems</a>
+          </div>
+          <div class="col-sm-4">
+            <h3 class="footer-heading">Navegação</h3>
+            <ul class="footer-links">
+              <li><a href="https://ebs-systems.epizy.com/">Home</a></li>
+              <li><a href="https://api-ebs-web.epizy.com/login/">Login</a></li>
+              <li><a href="https://api-ebs-web.epizy.com/singup/">Criar conta</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-4">
+            <h3 class="footer-heading">Contato</h3>
+            <ul class="footer-contact">
+              <li>E-mail: <a href="mailto:thiagosousa81@gmail.com">thiagosousa81@gmail.com</a></li>
+              <li>WhatsApp: <a href="https://wa.me/557588583604" target="_blank" rel="noopener noreferrer">+55 75 8858-3604</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
+            <p class="footer-text text-center">© 2023 EBS Security Systems. Todos os direitos reservados.</p>
+            <p class="footer-text text-center">Desenvolvido por Thiago Sousa</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!--Scripts externos-->
+    <script src="https://cdn.jsdelivr.net/npm/react@17.0.2/umd/react.development.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/react-dom@17.0.2/umd/react-dom.development.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-plugin-transform-react-jsx/6.24.1/transform-react-jsx.min.js"></script>
+
+        <!--Meus componentes-->
+    <script type="text/babel" src="/src/header.js" data-plugins="transform-react-jsx"></script>
+    <script type="text/babel" src="/src/navbar.php" data-plugins="transform-react-jsx"></script>
+    <!--<script type="text/babel" src="/src/React.js/section1.js" data-plugins="transform-react-jsx"></script>
+    
+    <script type="text/babel" src="/src/React.js/footer.js" data-plugins="transform-react-jsx"></script>-->
+    
+    <script type="text/babel" data-plugins="transform-react-jsx">
+            
+      ReactDOM.render(<Header />, document.getElementById('header'));
+      
+      ReactDOM.render(<NavBar />, document.getElementById('navbar'));
+      
+      //ReactDOM.render(<Section1 />, document.getElementById('first-message'));
+      
+      //ReactDOM.render(<Footer />, document.getElementById('footer'));
+      
+      
+    </script>
+  </body>
+</html>
