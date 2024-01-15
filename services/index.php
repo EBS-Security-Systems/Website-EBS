51a6b9aa21fbf47e7f06f944050cd318d878dedc
Thiago Sousa <thiagosousa81@gmail.com>
2024-01-15 15:37:55 -0300

    §

---
+++ services/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Serviços - EBS Seurity Systems</title>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="/images/NewEBSLogo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="/css/style.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <style>
+      .text-justify {
+        text-align: justify;
+      }
+      
+      .bg-light-gray {
+        background-color: lightgray;
+      }
+      .bg-white {
+        background-color: white;
+        padding-bottom: 5px;
+      }
+    </style>
+  </head>
+  <body>
+    <!--A aplicação será mostrada aqui-->
+    <?php include "../header.html"?>
+    
+    <section id="applications" class="jumbotron text-center mycss slide" style="margin-block: 0px; padding-block: 5px">
+        <h3 style="
+                text-align: center;
+                ">Acesso ilimitado às nossas aplicações</h3>
+        <h1 style="background-image: linear-gradient(0deg, black, green,black);"> EBS <img
+                src="https://ebs-systems.epizy.com/images/cropped-imgfinal.png"> WEB</h1>
+        <code style="
+                   -webkit-text-size-adjust: 125%;
+                   -webkit-tap-highlight-color: rgba(0,0,0,0);
+                   line-height: 1.42857143;
+                   text-align: center;
+                   box-sizing: border-box;
+                   font-family: Menlo,Monaco,Consolas,&quot;Courier New&quot;,monospace;
+                   padding: 2px 4px;
+                   font-size: 100%;
+                   color: green;
+                   background-color: #f9f2f4;
+                   border-radius: 4px;
+                   ">Encryption's Builder Studio na web!</code>
+        <div style="
+    margin-top: 5px;
+"><a href="https://web.ebs-systems.epizy.com/login"><button type="button"
+                    class="btn btn-secondary"><strong>LOGIN</strong></button></a> <a
+                href="https://web.ebs-systems.epizy.com/singup"><button type="button"
+                    class="btn btn-secondary"><strong>CADASTRE-SE</strong></button></a> <a
+                href="https://idc.ebs-systems.epizy.com/"><button type="button"
+                    class="btn btn-secondary"><strong>COMUNIDADE</strong></button></a></div>
+
+    
+    </section>
+    
+    
+    <section id="tercery-message" class="container-fluid" style="background-color: black; padding: 2%">
+        <h1 class="text-center text-light">Como podemos impulsionar o seu negócio?</h1>
+        <div class="container-fluid row">
+            <!-- Bloco 1 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Website Corporativo</h2>
+                <p class="text-justify">
+                    Apresente o seu negócio aos clientes com um website institucional, fornecendo informações de contato e páginas de conversão.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 2 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Agenda Online</h2>
+                <p class="text-justify">
+                    Gerencie os horários de forma eficiente com um sistema de agendamento automatizado, perfeito para barbearias, salões de beleza e lava-jatos.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 3 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Loja Virtual</h2>
+                <p class="text-justify">
+                    Venda seus produtos online com uma loja integrada a cadastros de clientes e produtos flexíveis.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 4 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Sistemas de Gestão</h2>
+                <p class="text-justify">
+                    Tenha um sistema de informação gerencial eficaz para cadastrar clientes, produtos, serviços e compras.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 5 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Integração entre Aplicações</h2>
+                <p class="text-justify">
+                    Acesse seus dados de aplicativos em dispositivos móveis com integração entre todos os nossos sistemas.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 6 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Aplicações de Mídias Sociais</h2>
+                <p class="text-justify">
+                    Crie redes sociais personalizadas, fóruns e comunidades online com recursos de interação social e segurança de assinatura digital.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 7 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Automações com Arduino</h2>
+                <p class="text-justify">
+                    Desenvolvemos produtos integrados a banco de dados online e equipamentos específicos para suas necessidades.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 8 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Automações para Web</h2>
+                <p class="text-justify">
+                    Automatize processos web, como transferência de dados do Excel para sistemas web e muito mais.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+
+            <!-- Bloco 9 -->
+            <div class="col-sm-4 bg-white">
+                <h2>Desenvolvimento e Integração com APIs</h2>
+                <p class="text-justify">
+                    Tenha sua própria API na web e integre nossos sistemas com outras APIs, garantindo a confidencialidade dos dados.
+                </p>
+                <a href="#first-message" class="btn btn-dark">Saiba mais</a>
+            </div>
+        </div>
+    </section>
+    
+    <style>
+    .cta-section {
+      background-color: green; /* Cor da seção CTA */
+      color: white;
+      text-align: center;
+      padding: 60px 0;
+    }
+
+    .cta-heading {
+      font-size: 2.5rem;
+      margin-bottom: 20px;
+    }
+
+    .cta-button {
+      font-size: 1.5rem;
+    }
+  </style>
+    
+    <section class="cta-section">
+    <div class="container">
+      <h2 class="cta-heading">Tudo isso e muito mais... Entre em contato!</h2>
+      <p class="lead">Estamos prontos para transformar sua experiência online. Não hesite em nos contatar para saber mais sobre nossos serviços e como podemos ajudar seu negócio a prosperar.</p>
+      <a href="https://wa.me/557588583604?text=Olá Thiago! Vi a sessão de serviços da sua startup e queria saber como vocês podem melhorar minha empresa." class="btn btn-light btn-lg cta-button">Entre em Contato</a>
+    </div>
+  </section>
+     
+     <?php include "../footer.html"?>
+    <div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>
+  </body>
+</html>
