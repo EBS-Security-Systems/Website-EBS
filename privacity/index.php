f437d55b138df40fe3a523c1a865d50b0d119df3
Thiago Sousa <thiagosousa81@gmail.com>
2024-01-26 15:09:35 -0300

    Create index.php

---
+++ privacity/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <title>Política de privacidade - EBS Seurity Systems</title>
+  <meta charset="utf-8">
+  <link rel="shortcut icon" href="/images/NewEBSLogo.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Place this tag in your head or just before your close body tag. -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="/css/style.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <style>
+    .text-justify {
+      text-align: justify;
+    }
+
+    .bg-light-gray {
+      background-color: lightgray;
+    }
+
+    .bg-white {
+      background-color: white;
+      padding-bottom: 5px;
+    }
+  </style>
+</head>
+
+<body style="max-width: 100%">
+
+  <?php include "../header.html" ?>
+
+  <!-- Início do conteúdo da página -->
+  <div class="p-5">
+    <section class="container-fluid bg-white p-3 rounded">
+
+      <h1 class="text-center">Política de Privacidade</h1>
+      <h2 class="text-center">
+        EBS Security Systems
+      </h2>
+
+      <h6 class="lead">Última atualização: 26 de Janeiro de 2024</h6>
+      <hr style="border: 0; border-top: 3px solid black;">
+
+      <h3><b>1 - Informações pessoais que coletamos</b></h3>
+
+      <p>Coletamos informações pessoais como nome, endereço de e-mail e informações demográficas quando você se inscreve
+        em nossa newsletter ou faz uma compra em nosso site. Também podemos coletar informações sobre como você usa o
+        nosso site, como páginas visitadas e tempo gasto em cada página.</p>
+
+      <h3><b>2 - Cookies e outras tecnologias de rastreamento</b></h3>
+
+      <p>Utilizamos cookies e outras tecnologias de rastreamento para personalizar a sua experiência no nosso site e
+        para
+        coletar informações sobre como você interage com o site. Essas informações nos ajudam a melhorar o conteúdo e a
+        funcionalidade do nosso site. Você pode optar por não permitir o uso de cookies alterando as configurações do
+        seu
+        navegador.</p>
+
+      <h3><b>3 - Uso das informações pessoais</b></h3>
+
+      <p>As informações pessoais que coletamos são utilizadas para processar pedidos, enviar newsletters e melhorar a
+        experiência do usuário no nosso site. Também podemos utilizar essas informações para entrar em contato com você
+        sobre produtos e serviços relacionados ao nosso negócio.</p>
+
+      <h3><b>4 - Divulgação de informações pessoais</b></h3>
+
+      <p>Não vendemos, trocamos ou transferimos as suas informações pessoais para terceiros sem o seu consentimento,
+        exceto quando exigido por lei ou para cumprir com obrigações contratuais.</p>
+
+      <h3><b>5 - Segurança das informações pessoais</b></h3>
+
+      <p>Implementamos medidas de segurança para proteger as suas informações pessoais contra o acesso não autorizado ou
+        uso indevido. No entanto, nenhuma transmissão de dados pela internet é 100% segura. Sendo assim, não podemos
+        garantir a segurança total das informações enviadas por meio do nosso site.</p>
+
+      <h3><b>6 - Links para outros sites</b></h3>
+
+      <p>O nosso site pode conter links para outros sites que não são controlados por nós. Esta política de privacidade
+        se
+        aplica apenas ao nosso site, sendo assim, recomendamos que você leia as políticas de privacidade dos sites que
+        visitar.</p>
+
+      <h3><b>7 - Alterações na Política de Privacidade</b></h3>
+
+      <p>Reservamos o direito de modificar esta Política de Privacidade a qualquer momento. É de sua responsabilidade
+        verificar periodicamente esta página para atualizações.</p>
+
+      <h3><b>8 - Contato</b></h3>
+
+      <p>Se tiver alguma dúvida sobre esta Política de Privacidade ou sobre como lidamos com informações pessoais, entre
+        em contato conosco através do e-mail <a href="mailto:thiagosousa81@gmail.com">thiagosousa81@gmail.com</a>.</p>
+
+      <p>Agradecemos a sua confiança no nosso site, pois fazemos o possível para garantir a segurança e privacidade das
+        suas informações pessoais através das nossas aplicações de segurança.</p>
+
+    </section>
+  </div>
+  <!-- Fim do conteúdo da página -->
+  <?php include "../footer.html" ?>
+  <div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>
+</body>
+
+</html>
