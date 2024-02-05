0249876111a32fffedd6380469f51419d1d29a3a
Thiago Sousa <thiagosousa81@gmail.com>
2024-02-05 12:04:32 -0300

    §

---
+++ error/400/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <title>Erro - EBS Seurity Systems</title>
+  <meta charset="utf-8">
+  <link rel="shortcut icon" href="/images/NewEBSLogo.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Place this tag in your head or just before your close body tag. -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="/css/style.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <style>
+    .text-justify {
+      text-align: justify;
+    }
+
+    .bg-light-gray {
+      background-color: lightgray;
+    }
+
+    .bg-white {
+      background-color: white;
+      padding-bottom: 5px;
+    }
+  </style>
+</head>
+
+<body>
+  <!--A aplicação será mostrada aqui-->
+  <?php include "../../header.php" ?>
+
+  <div class="container mt-5 mb-5" style="height: 30vh">
+
+    <div class="jumbotron text-center text-white">
+      <h1>
+        Erro #400
+      </h1>
+      <h3>
+        Solicitação inválida!<br> <a style="color: white" href="/">Página inicial</a>
+      </h3>
+    </div>
+
+  </div>
+
+  </div>
+  <?php include "../../footer.php" ?>
+  <div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>
+</body>
+
+</html>
