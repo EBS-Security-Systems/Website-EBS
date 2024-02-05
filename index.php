4a57575f65dbfcae9186b0302e93180c9ce8e253
Thiago Sousa <thiagosousa81@gmail.com>
2024-02-05 11:51:04 -0300

    §

---
+++ index.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="https://ebs-systems.epizy.com/images/NewEBSLogo.png" />
+    <meta property="og:image:secure_url" content="https://ebs-systems.epizy.com/images/NewEBSLogo.png" />
+    <meta property="og:image:type" content="image/png" /> <!-- ou "image/png" ou o tipo de imagem apropriado -->
+    <meta property="og:image:width" content="600" /> <!-- Largura da imagem em pixels -->
+    <meta property="og:image:height" content="400" /> <!-- Altura da imagem em pixels -->
+
+    <!-- Título e descrição Open Graph (substitua pelo seu conteúdo) -->
+    <meta property="og:title" content="EBS Security Systems" />
+    <meta property="og:description" content="Instituição de serviços digitais" />
+
+
+    <title>EBS Seurity Systems</title>
+
+    <meta name="application-name" content="EBS Security Systems">
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="/images/NewEBSLogo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="/css/style.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+
+    <section id="carouselExampleIndicators" class="carousel slide carousel-dark" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                data-bs-interval="2000" class="active" aria-label="Diapositivo 1" aria-current="true"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                data-bs-interval="2000" aria-label="Diapositivo 2" class=""></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                data-bs-interval="2000" aria-label="Diapositivo 3" class=""></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+
+                <img src="/images/slide/1.png" class="d-block w-100" alt="Slide 1">
+
+                <div class="carousel-caption d-md-block">
+                    <h1>Digitalizamos sua empresa</h1>
+                    <h3>Buscamos trasnformar o manual em digital</h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/images/slide/2.png" class="d-block w-100" alt="Slide 2">
+                <div class="carousel-caption d-md-block">
+                    <h1>Desenvolvemos de tudo</h1>
+                    <h3>Desde sites a sistemas diversos integrados</h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/images/slide/3.png" class="d-block w-100" alt="Slide 3">
+                <div class="carousel-caption d-md-block">
+                    <h1>Um projeto sob medida para você</h1>
+                    <h3>Sua solução é nossa prioridade</h3>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">Anterior</font>
+                </font>
+            </span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">Próximo</font>
+                </font>
+            </span>
+        </button>
+    </section>
+
+
+
+
+    <section id="second-message" class="container-fluid bg-dark text-white"
+        style="background-color: white; padding-block: 15px;">
+        <h2 class="text-center">Em nosso serviço você vai encontrar</h2>
+        <div class="row">
+            <div class="col-sm-3"
+                style="display: flex; flex-direction: row; justify-content: initial; align-items: center;">
+                <svg style="width: 15%;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    aria-hidden="true" role="img" class="iconify iconify--carbon" width="100%" height="100%"
+                    preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
+                    <path fill="currentColor"
+                        d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm-2 19.59l-5-5L10.59 15L14 18.41L21.41 11l1.596 1.586Z">
+                    </path>
+                    <path fill="none" d="m14 21.591l-5-5L10.591 15L14 18.409L21.41 11l1.595 1.585L14 21.591z"></path>
+                </svg>
+                <h4 style="margin-inline-start: 10px;">Segurança</h4>
+            </div>
+            <div class="col-sm-3"
+                style="display: flex; flex-direction: row; justify-content: initial; align-items: center;">
+                <svg style="width: 15%;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    aria-hidden="true" role="img" class="iconify iconify--carbon" width="100%" height="100%"
+                    preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
+                    <path fill="currentColor"
+                        d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm-2 19.59l-5-5L10.59 15L14 18.41L21.41 11l1.596 1.586Z">
+                    </path>
+                    <path fill="none" d="m14 21.591l-5-5L10.591 15L14 18.409L21.41 11l1.595 1.585L14 21.591z"></path>
+                </svg>
+                <h4 style="margin-inline-start: 10px;">Confiabilidade</h4>
+            </div>
+            <div class="col-sm-3"
+                style="display: flex; flex-direction: row; justify-content: initial; align-items: center;">
+                <svg style="width: 15%;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    aria-hidden="true" role="img" class="iconify iconify--carbon" width="100%" height="100%"
+                    preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
+                    <path fill="currentColor"
+                        d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm-2 19.59l-5-5L10.59 15L14 18.41L21.41 11l1.596 1.586Z">
+                    </path>
+                    <path fill="none" d="m14 21.591l-5-5L10.591 15L14 18.409L21.41 11l1.595 1.585L14 21.591z"></path>
+                </svg>
+                <h4 style="margin-inline-start: 10px;">Versátilidade</h4>
+            </div>
+            <div class="col-sm-3"
+                style="display: flex; flex-direction: row; justify-content: initial; align-items: center;">
+                <svg style="width: 15%;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    aria-hidden="true" role="img" class="iconify iconify--carbon" width="100%" height="100%"
+                    preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
+                    <path fill="currentColor"
+                        d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm-2 19.59l-5-5L10.59 15L14 18.41L21.41 11l1.596 1.586Z">
+                    </path>
+                    <path fill="none" d="m14 21.591l-5-5L10.591 15L14 18.409L21.41 11l1.595 1.585L14 21.591z"></path>
+                </svg>
+                <h4 style="margin-inline-start: 10px;">Desenvolvimento</h4>
+            </div>
+        </div>
+    </section>
+
+    <section id="tercery-message" class="container-fluid" style="background-color: black;padding-block: 2%">
+
+        <h1 class="text-center text-light">
+            Como podemos melhorar seu negócio?
+        </h1>
+        <h3 class="text-center text-light">
+            Entre em <a href="#footer" class="text-white">contato</a> para saber mais
+        </h3>
+        <div class="container-fluid row">
+            <div class="col-sm-4 bg-white p-2">
+                <h2>
+                    Website corporativo
+                </h2>
+                <p class="text-justify">
+                    Melhore sua presença digital para clientes conhecerem melhor sobre seu negócio com um website
+                    corporativo com
+                    informações de contato e
+                    páginas de conversão.
+                </p>
+
+                <a href="/services"><button class="btn btn-dark">
+                        Saiba mais
+                    </button></a>
+            </div>
+            <div class="col-sm-4 bg-white p-2">
+                <h2>
+                    Agenda online
+                </h2>
+                <p class="text-justify">
+                    Um sistema de agendamento totalmente autoatizado! Gerencia os horários de forma automatizada e
+                    eficaz. Perfeito para barbearias, salões de beleza e até lava-jato.
+                </p>
+
+                <a href="/services"><button class="btn btn-dark">
+                        Saiba mais
+                    </button></a>
+            </div>
+            <div class="col-sm-4 bg-white p-2">
+                <h2>
+                    Loja virtual
+                </h2>
+                <p class="text-justify">
+                    Venda seu produto sem sair de casa! Sua loja é integrada com um cadastro de clientes e um cadastro
+                    de produtos
+                    onde você pode mudar o que quiser.
+                </p>
+
+                <a href="/services"><button class="btn btn-dark">
+                        Saiba mais
+                    </button></a>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="ebs-section">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-4" style="
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    align-content: center;
+    justify-content: center;
+">
+                    <img src="/images/NewEBSLogo.png" alt="EBS Logo" class="ebs-logo" style="">
+                </div>
+                <div class="col-md-8">
+                    <h1 class="mb-8">Quem Somos</h1>
+                    <p class="lead">A <a href="/" class="text-white">EBS Security Systems</a> é uma startup
+                        especializada no
+                        desenvolvimento de soluções de software inovadoras e seguras. Nossa missão é oferecer produtos
+                        que protejam
+                        a privacidade e a segurança dos usuários online, contribuindo para um ambiente virtual mais
+                        confiável.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="quart-message" class="container-fluid" style="background-color: white">
+
+        <div class="container-fluid row" style="padding-inline-start: 30px">
+            <div class="col-sm-4" style="background-color: black;padding-block: 20px;">
+                <h2 style="color: white">
+                    Missão
+                </h2>
+                <p style="color: white; padding-inline: 15px;" class="text-justify">
+                    A <a href="#" style="color: white"><strong>EBS Security Systems</strong></a> tem como missão
+                    oferecer soluções inovadoras e eficazes para proteger a privacidade e segurança dos usuários na
+                    internet, garantindo um ambiente virtual mais seguro e confiável.
+                </p>
+            </div>
+            <div class="col-sm-4" style="
+    padding-block: 20px;
+">
+                <h2>
+                    Visão
+                </h2>
+
+                <p style="color: #303030; padding-inline: 15px;" class="text-justify">
+                    Acreditamos que todos têm direito a um ambiente virtual seguro e livre de ameaças. Queremos ser
+                    referência em <a href="https://github.com/EBS-Security-Systems/" style="color: black"><b>segurança
+                            cibernética</b>,</a> trabalhando em conjunto com nossos colaboradores para alcançar esse
+                    objetivo.
+                </p>
+            </div>
+            <div class="col-sm-4" style="background-color: black;padding-block: 20px;">
+                <h2 style="color: white">
+                    Valores
+                </h2>
+
+                <p style="color: white; padding-inline: 15px;" class="text-justify">
+                    Somos uma instituição comprometida com a transparência, ética, responsabilidade social e inovação.
+                    <a href="/services" style="color: white"><strong>Nossos serviços</strong></a> são baseados na
+                    qualidade, confiabilidade e excelência no atendimento ao cliente.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid text-white">
+        <h1 class="text-center">Equipe EBS</h1>
+        <div class="row">
+            <div class="col-sm-4 text-center">
+                <img src="/images/colaborators/Profile.png" alt="" class="perfil">
+                <h3><a target="_blank" href="https://thiagosousa81.wordpress.com/" class="text-white">Thiago Sousa</a>
+                </h3>
+                <p>CEO da EBS Systems <br> Técnico em Informática <br> Dev. Full-Stack <br> Hacker Ético </p>
+            </div>
+            <div class="col-sm-4 text-center">
+                <img src="/images/NewEBSLogo.png" alt="" class="perfil">
+                <h3>Fernando Junior</h3>
+                <p>CEO da Fenix Line
+                    <br>Técnico em Informática
+                    <br>Gerente Administrativo
+                </p>
+            </div>
+            <div class="col-sm-4 text-center">
+                <img src="/images/NewEBSLogo.png" alt="" class="perfil">
+                <h3>João Victor</h3>
+                <p>Técnico em Informática
+                    <br>Dev. Full-Stack
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4 text-center">
+                <img src="/images/NewEBSLogo.png" alt="" class="perfil">
+                <h3>Rafaela Almeida</h3>
+                <p>Técnica em Informática
+                    <br>Gerente de Marketing
+                </p>
+            </div>
+            <div class="col-sm-4 text-center">
+                <img src="/images/NewEBSLogo.png" alt="" class="perfil">
+                <h3>Vinicius Cerqueira</h3>
+                <p>Técnico em Informática<br>Desing gráfico</p>
+            </div>
+            <div class="col-sm-4 text-center">
+                <img src="/images/NewEBSLogo.png" alt="" class="perfil">
+                <h3>Felipe Buri</h3>
+                <p>Dev. Front-End</p>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section id="applications" class="jumbotron text-center mycss slide container-fluid"
+        style="margin-block: 0px; padding-block: 5px;">
+        <div class="row">
+            <div class="col-sm-6 bg-dark rounded p-3"
+                style="display: flex; justify-content: center; align-items: center; flex-direction: column; border-style: double;">
+                <h4 class="text-center text-white">A EBS Security Systems também desenvolve projetos e métodos
+                    científicos
+                    para melhorar a
+                    cibersegurança</h4>
+                <a href="/applications/"><button type="button" class="btn btn-secondary"><strong>VER
+                            APLICAÇÕES</strong></button></a>
+            </div>
+
+            <div class="col-sm-6">
+                <h3 class="text-center">Acesso ilimitado às nossas aplicações</h3>
+                <h1 style="background-image: linear-gradient(0deg, black, green,black);"> EBS <img
+                        src="/images/cropped-imgfinal.png"> WEB</h1>
+                <code style="
+                     -webkit-text-size-adjust: 125%;
+                     -webkit-tap-highlight-color: rgba(0,0,0,0);
+                     line-height: 1.42857143;
+                     text-align: center;
+                     box-sizing: border-box;
+                     font-family: Menlo,Monaco,Consolas,&quot;Courier New&quot;,monospace;
+                     padding: 2px 4px;
+                     font-size: 100%;
+                     color: green;
+                     background-color: #f9f2f4;
+                     border-radius: 4px;
+                     ">Encryption's Builder Studio na web!</code>
+                <div style="
+      margin-top: 5px;
+  "><a href="https://web.ebs-systems.epizy.com/login"><button type="button"
+                            class="btn btn-secondary"><strong>LOGIN</strong></button></a> <a
+                        href="https://web.ebs-systems.epizy.com/singup"><button type="button"
+                            class="btn btn-secondary"><strong>CADASTRE-SE</strong></button></a> <a
+                        href="https://idc.ebs-systems.epizy.com/"><button type="button"
+                            class="btn btn-secondary"><strong>COMUNIDADE</strong></button></a></div>
+            </div>
+        </div>
+    </section>
+
+
+    <?php include "footer.php"; ?>
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
+</body>
+
+</html>
